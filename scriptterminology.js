401461892dfe720e56107289ba6ebe348e7795f9
Joshuanathan <joshuanatham16@gmail.com>
2023-06-02 23:22:10 +0800

    first commit

---
+++ scriptterminology.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+}
